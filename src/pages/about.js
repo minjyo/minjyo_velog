ac89c7abc48b04c27a5899f611d41301e3d40ef7
minjyo <minjyo8823@gmail.com>
2020-02-18 13:44:42 +0900

    Gatsby tutorial
    1. src/styles - glocal.css & ./ - gatsby-browser.js
    2. .moduel.css -> import styles
    3. gatsby plugin - src/utils - typography.js
    4. src/components - layout.js -> link navigator

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,11 +1,14 @@
 import React from "react"
 import Header from "../components/header"
 import { Link } from "gatsby"
+import Layout from "../components/layout"
 
 export default () => (
-  <div style={{ color: `teal` }}>
-    <Link to="/contact/">Contact</Link>
-    <Header headerText="About Gatsby" />
-    <p>Such wow. Very React.</p>
-  </div>
+  <Layout>
+    <div style={{ color: `teal` }}>
+      <Link to="/contact/">Contact</Link>
+      <Header headerText="About Gatsby" />
+      <p>Such wow. Very React.</p>
+    </div>
+  </Layout>
 )
